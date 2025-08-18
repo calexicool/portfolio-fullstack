fcae89498f81062b3370e223b749050087bc4bdf
calexicool <35396228+calexicool@users.noreply.github.com>
2025-08-18 19:46:28 +0300

    18

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,12 @@
+// frontend/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const BASE = process.env.BUILD_TARGET === 'pages'
+  ? '/portfolio-fullstack/'   // имя репозитория для GH Pages
+  : '/';                      // Render/локально
+
 export default defineConfig({
-  base: '/portfolio-fullstack/',        // один домен
+  base: BASE,
   plugins: [react()],
 })
